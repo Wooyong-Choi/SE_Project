8156a81df0c374d08f4cd5f1c7c85bf20596fae7
WooYong <WooYong@PC>
2017-06-20 01:25:39 +0900

    Event Control

--- SE_Project/src/gui/Event_Control.java
+++ SE_Project/src/gui/Event_Control.java
@@ -18,181 +18,183 @@ import ocsf.Packet;
 import ocsf.SocketCommunication;
 
 public class Event_Control extends SocketCommunication implements Runnable, ActionListener{
-	
-	private JButton login_btn;
-	private JButton cur_btn;
-	private JButton sche_btn;
-	private JButton mem_btn;
-	private JButton change_btn;
-	private JButton open_btn;
-	private Login_Layout ll;
-	private String btn_String;
-	private TextField id_field, pass_field;
-	private Main_Layout ml;
-	private JFrame main_frame;
-	private JPanel login_main_panel;
-	private JPanel main_main_panel;
-	private Current_Menu_Panel current_menu;
-	private Schedule_Menu_Panel schedule_menu;
-	private boolean check;
-	private JPanel change_pane;
-	private Info_Change_Dialog icd;
-	private Open_Menu_Panel omp;
-	
-	
-	public Event_Control() {
-		this.check = true;
-		this.ll = new Login_Layout();
-		this.ml = new Main_Layout();
-		this.login_btn = ll.getLoginBtn();
-		this.id_field = ll.getUserId();
-		this.pass_field = ll.getUserPass();
-		this.mem_btn = ll.getMemberBtn();
-		
-		this.cur_btn = ml.getCurrentBtn();
-		this.sche_btn = ml.getScheduleBtn();
-		
-		this.current_menu = new Current_Menu_Panel();
-		this.schedule_menu = new Schedule_Menu_Panel();
-		
-		open_btn = schedule_menu.getOpenLectureBtn();
-		
-		login_main_panel = ll.getMainPanel();
-		main_main_panel = ml.getMainPanel();
-		
-		//시작은 수강 현황 조회하기로 시작.
-		change_pane = ml.getMainPanel();
-		change_pane.add(current_menu.getContentsPanel());
-		
-		cur_btn.addActionListener(this);
-		sche_btn.addActionListener(this);
-		login_btn.addActionListener(this);
-		id_field.addActionListener(this);
-		pass_field.addActionListener(this);
-		open_btn.addActionListener(this);
-		mem_btn.addActionListener(this);
-		
-		try {
-			this.sock = new Socket("127.0.0.1", Common.portNum);
-			
-			boolean result = sock.isConnected();
-			if (result) {
-				System.out.println("Success");	
-			}
-			else
-				System.err.println("Fail");
-		} 
-		catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+   
+   private JButton login_btn;
+   private JButton cur_btn;
+   private JButton sche_btn;
+   private JButton mem_btn;
+   private JButton change_btn;
+   private JButton open_btn;
+   private Login_Layout ll;
+   private String btn_String;
+   private TextField id_field, pass_field;
+   private Main_Layout ml;
+   private JFrame main_frame;
+   private JPanel login_main_panel;
+   private JPanel main_main_panel;
+   private Current_Menu_Panel current_menu;
+   private Schedule_Menu_Panel schedule_menu;
+   private boolean check;
+   private JPanel change_pane;
+   private Info_Change_Dialog icd;
+   private Open_Menu_Panel omp;
+   
+   private Student std;
+   
+   
+   public Event_Control() {
+      this.check = true;
+      this.ll = new Login_Layout();
+      this.ml = new Main_Layout();
+      this.login_btn = ll.getLoginBtn();
+      this.id_field = ll.getUserId();
+      this.pass_field = ll.getUserPass();
+      this.mem_btn = ll.getMemberBtn();
+      
+      this.cur_btn = ml.getCurrentBtn();
+      this.sche_btn = ml.getScheduleBtn();
+      
+      this.current_menu = new Current_Menu_Panel(std);
+      this.schedule_menu = new Schedule_Menu_Panel();
+      
+      open_btn = schedule_menu.getOpenLectureBtn();
+      
+      login_main_panel = ll.getMainPanel();
+      main_main_panel = ml.getMainPanel();
+      
+      //시작은 수강 현황 조회하기로 시작.
+      change_pane = ml.getMainPanel();
+      change_pane.add(current_menu.getContentsPanel());
+      
+      cur_btn.addActionListener(this);
+      sche_btn.addActionListener(this);
+      login_btn.addActionListener(this);
+      id_field.addActionListener(this);
+      pass_field.addActionListener(this);
+      open_btn.addActionListener(this);
+      mem_btn.addActionListener(this);
+      
+      try {
+         this.sock = new Socket("127.0.0.1", Common.portNum);
+         
+         boolean result = sock.isConnected();
+         if (result) {
+            System.out.println("Success");   
+         }
+         else
+            System.err.println("Fail");
+      } 
+      catch (UnknownHostException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      } 
+      catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 
-	@Override
-	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
-		btn_String = new String();
-		btn_String = event.getActionCommand();
-		
-		if (btn_String.equals("정보변경")) {
-			System.out.println("정보변경");
-			icd = new Info_Change_Dialog();
-			change_btn = icd.getChange_btn();
-			change_btn.addActionListener(this);
-		}
-		
-		if (btn_String.equals("변경하기")) {
-			System.out.println("변경하기");
-			
-			Student usr = new Student(icd.getBefore_id_field().getText(), icd.getBefore_pass_field().getText());
-			Packet pkt = new Packet(usr, 0);
-			writeToSocket(pkt);
-			pkt = readFromSocket();
-			
-			// 로그인 성공
-			if (pkt.getRequest() != -1) {
-				Student std = pkt.getStd();
-				std.setID(icd.getChange_id_field().getText());
-				std.setPassword(icd.getChange_pass_field().getText());
-				
-				pkt = new Packet(std, 1);
-				writeToSocket(pkt);
-				
-				icd.setVisible(false);
-			}
-			// 로그인 실패
-			else {
-				System.out.println("실패");
-			}
-		}
-		
-		if (btn_String.equals("수강현황 조회")) {
-			System.out.println("수강현황 조회");
-			change_pane.remove(1);
-			change_pane.add(current_menu.getContentsPanel());
-			change_pane.updateUI();
-		}
-		
-		if (btn_String.equals("시간표 관리")) {
-			System.out.println("시간표 관리하기");
-			change_pane.remove(1);
-			change_pane.add(schedule_menu.getSchedulePanel());
-			change_pane.updateUI();
-		}
-		
-		if (btn_String.equals("개설강좌 조회")) {
-			System.out.println("개설강좌 조회");
-			omp = new Open_Menu_Panel();
-		}
-		
-		if (btn_String.equals("  로그인  ")) {
-			Student usr = new Student(id_field.getText(), pass_field.getText());
-			Packet pkt = new Packet(usr, 0);
-			writeToSocket(pkt);
-			pkt = readFromSocket();
-			
-			// 로그인 성공
-			if (pkt.getRequest() != -1) {
-				System.out.println("로그인 성공");
-				
-				ml.setStdInfo(pkt.getStd());
-				
-				ll.getMainFrame().setVisible(false);
-				ml.getMainFrame().setVisible(true);
-				check = false;
-			}
-			// 로그인 실패
-			else {
-				System.out.println("실패");
-			}
-		}
-	}
+   @Override
+   public void actionPerformed(ActionEvent event) {
+      // TODO Auto-generated method stub
+      btn_String = new String();
+      btn_String = event.getActionCommand();
+      
+      if (btn_String.equals("정보변경")) {
+         System.out.println("정보변경");
+         icd = new Info_Change_Dialog();
+         change_btn = icd.getChange_btn();
+         change_btn.addActionListener(this);
+      }
+      
+      if (btn_String.equals("변경하기")) {
+         System.out.println("변경하기");
+         
+         Student usr = new Student(icd.getBefore_id_field().getText(), icd.getBefore_pass_field().getText());
+         Packet pkt = new Packet(usr, 0);
+         writeToSocket(pkt);
+         pkt = readFromSocket();
+         
+         // 로그인 성공
+         if (pkt.getRequest() != -1) {
+            Student std = pkt.getStd();
+            std.setID(icd.getChange_id_field().getText());
+            std.setPassword(icd.getChange_pass_field().getText());
+            
+            pkt = new Packet(std, 1);
+            writeToSocket(pkt);
+            
+            icd.setVisible(false);
+         }
+         // 로그인 실패
+         else {
+            System.out.println("실패");
+         }
+      }
+      
+      if (btn_String.equals("수강현황 조회")) {
+         System.out.println("수강현황 조회");
+         change_pane.remove(1);
+         change_pane.add(current_menu.getContentsPanel());
+         change_pane.updateUI();
+      }
+      
+      if (btn_String.equals("시간표 관리")) {
+         System.out.println("시간표 관리하기");
+         change_pane.remove(1);
+         change_pane.add(schedule_menu.getSchedulePanel());
+         change_pane.updateUI();
+      }
+      
+      if (btn_String.equals("개설강좌 조회")) {
+         System.out.println("개설강좌 조회");
+         omp = new Open_Menu_Panel();
+      }
+      
+      if (btn_String.equals("  로그인  ")) {
+         Student usr = new Student(id_field.getText(), pass_field.getText());
+         Packet pkt = new Packet(usr, 0);
+         writeToSocket(pkt);
+         pkt = readFromSocket();
+         
+         // 로그인 성공
+         if (pkt.getRequest() != -1) {
+            System.out.println("로그인 성공");
+            std = pkt.getStd();
+            ml.setStdInfo(std);
+            
+            ll.getMainFrame().setVisible(false);
+            ml.getMainFrame().setVisible(true);
+            check = false;
+         }
+         // 로그인 실패
+         else {
+            System.out.println("실패");
+         }
+      }
+   }
 
-	@Override
-	public void run() {
-		while (check) {
-		// TODO Auto-generated method stub
-			if (id_field.getText().equals("") || pass_field.getText().equals(""))
-				login_btn.setEnabled(false);
-			else
-				login_btn.setEnabled(true);
-			
-			try {
-				Thread.sleep(100);
-			}
-			
-			catch (Exception e){
-				
-			}
-		}
-	}
-	
-	public Main_Layout getMainLayout() {
-		return ml; 
-	}
+   @Override
+   public void run() {
+      while (check) {
+      // TODO Auto-generated method stub
+         if (id_field.getText().equals("") || pass_field.getText().equals(""))
+            login_btn.setEnabled(false);
+         else
+            login_btn.setEnabled(true);
+         
+         try {
+            Thread.sleep(100);
+         }
+         
+         catch (Exception e){
+            
+         }
+      }
+   }
+   
+   public Main_Layout getMainLayout() {
+      return ml; 
+   }
 
 }
